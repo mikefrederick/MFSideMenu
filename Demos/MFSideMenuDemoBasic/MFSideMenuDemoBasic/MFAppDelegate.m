86f6ff87e35ca9cfc3b560595313db5b1fd48550
Michael Frederick <mike@viamike.com>
2013-03-28 07:30:48 -0400

    Make the demo side menus slide w/ root controller by default

--- Demos/MFSideMenuDemoBasic/MFSideMenuDemoBasic/MFAppDelegate.m
+++ Demos/MFSideMenuDemoBasic/MFSideMenuDemoBasic/MFAppDelegate.m
@@ -30,7 +30,7 @@
     MFSideMenu *sideMenu = [MFSideMenu menuWithNavigationController:navigationController
                                             leftSideMenuController:leftSideMenuController
                                             rightSideMenuController:rightSideMenuController];
-    
+    sideMenu.menuSlideAnimationEnabled = YES;
     leftSideMenuController.sideMenu = sideMenu;
     rightSideMenuController.sideMenu = sideMenu;
     
