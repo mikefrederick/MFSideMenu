3e20b9eb9ca03432b3da6badc5c0a908d6c55043
Michael Frederick <mike@viamike.com>
2013-05-05 09:19:11 -0400

    Helper file for importing MFSideMenuContainerViewController

--- MFSideMenu/MFSideMenu.h
+++ MFSideMenu/MFSideMenu.h
@@ -3,11 +3,7 @@
 //  MFSideMenuDemoBasic
 //
 //  Created by Michael Frederick on 5/5/13.
-//  Copyright (c) 2013 University of Wisconsin - Madison. All rights reserved.
+//  Copyright (c) 2013 Frederick Development. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
-
-@protocol MFSideMenu <NSObject>
-
-@end
+#import "MFSideMenuContainerViewController.h"
