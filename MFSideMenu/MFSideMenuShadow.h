6f83aa3e85733cd127c4c885f16875c14cb1d64d
Mike Frederick, mikefrederick <pair+mike@gust.com>
2013-08-14 13:19:41 -0400

    Separated shadow out into it's own object

---
+++ MFSideMenu/MFSideMenuShadow.h
@@ -0,0 +1,26 @@
+//
+//  MFSideMenuShadow.h
+//  MFSideMenuDemoSearchBar
+//
+//  Created by Michael Frederick on 5/13/13.
+//  Copyright (c) 2013 Frederick Development. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MFSideMenuShadow : NSObject
+
+@property (nonatomic, assign) BOOL enabled;
+@property (nonatomic, assign) CGFloat radius;
+@property (nonatomic, assign) CGFloat opacity;
+@property (nonatomic, strong) UIColor *color;
+@property (nonatomic, weak) UIView *shadowedView;
+
++ (MFSideMenuShadow *)shadowWithView:(UIView *)shadowedView;
++ (MFSideMenuShadow *)shadowWithColor:(UIColor *)color radius:(CGFloat)radius opacity:(CGFloat)opacity;
+
+- (void)draw;
+- (void)shadowedViewWillRotate;
+- (void)shadowedViewDidRotate;
+
+@end
